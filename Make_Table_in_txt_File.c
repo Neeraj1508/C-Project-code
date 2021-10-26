6f61af5ab4830d5bf5f37cdf4df6b2291b6153c5
Neeraj Fullara <75372853+Neeraj1508@users.noreply.github.com>
2021-10-26 21:22:33 +0530

    Update Make_Table_in_txt_File.c

--- Make_Table_in_txt_File.c
+++ Make_Table_in_txt_File.c
@@ -7,7 +7,7 @@ int main()
     printf("Enter the Integer of table which you need\n");
     scanf("%d",&i);
     int mul;
-    ptr = fopen("Practice gen.txt", "w");
+    ptr = fopen("Table.txt", "w");
     while (x < 11)
     {
         mul = i*x;
@@ -16,6 +16,6 @@ int main()
     }
     fclose(ptr);
     printf("Done\n");
-    printf("Now Go and check The txt file of name Practice gen.txt for table\n");
+    printf("Now Go and check The txt file of name Table.txt for table in your Current Folder\n");
     return 0;
 }
